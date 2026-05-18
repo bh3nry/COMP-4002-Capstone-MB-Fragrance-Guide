9108f041ecab9c5c8c49d02c4375cffdb6792471
David Chan <dchan51@rrc.ca>
2026-05-18 12:39:06 -0500

    feat: created notes-styles.css for NoteTable component.

--- src/components/notes/noteList.tsx
+++ src/components/notes/noteList.tsx
@@ -1,10 +1,18 @@
 import type { Notes } from "./notes-data";
+import "./notes-styles.css";
 
 type FragNotesProps = {
     notes: Notes;
     pics: string;
 };
 
+/* Component for displaying fragrance notes in a list format
+    @params: 
+    notes: an object containing the name, description, and suggestions
+    for a fragrance note
+    pics: a string representing the path to an image associated with the note
+    @returns: a JSX element that renders the note's name, description, and suggestions
+*/
 function NoteTable({
     notes,
     pics
@@ -14,20 +22,20 @@ function NoteTable({
     const suggestions = notes.suggestions;
 
     return (
-        <ul>
-            <li>
+        <ul className="custom-list">
+            <li className="list-row">
                 <div><img src={pics} alt={name} /></div>
-                <div>{name}</div>
-                <p>{description}</p>
+                <div className="note-name">{name}</div>
+                <p className="description">{description}</p>
                 <details className="dropdown">
                     <summary>Fragrance Suggestions</summary>
-                    <ul>
+                    <ul className="menu dropdown-content">
                         <li>{suggestions}</li>
                     </ul>
                 </details>
             </li>
         </ul>
     )
-}
+};
 
 export default NoteTable;
