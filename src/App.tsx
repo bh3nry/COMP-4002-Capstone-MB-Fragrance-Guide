fbebee24b739f46728d86226b39341edc1296df4
Bren Henry <brendynfunk@gmail.com>
2026-05-18 21:02:31 -0500

    feat: fixing merge conflicts between SearchBar and ProductCard

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
 import ProductCard from './components/ProductCard/ProductCard'
 import { testPerfume } from './components/ProductCard/product-data-test'
 import SearchBar from './components/searchbar/SearchBar' 
-import ProductCard from './components/ProductCard/ProductCard'
-import { testPerfume } from './components/ProductCard/product-data-test'
 import './App.css'
 
 const App = () => {
 
   return (
     <>
-      <ProductCard 
-        perfumeObj={testPerfume}
-      />
       <SearchBar
         onSearch={(query) => console.log("query:", query)}
       />
