017348287ae5ad065558a7aab5c8839d935f7f34
David Chan <dchan51@rrc.ca>
2026-05-18 21:22:43 -0500

    feat: Implemented table for notes in app.tsx.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import ProductCard from './components/ProductCard/ProductCard'
 import { testPerfume } from './components/ProductCard/product-data-test'
 import SearchBar from './components/searchbar/SearchBar' 
+import NoteTable from './components/notes/noteList'
+import { Patchouli, Sandalwood, Juniper } from './components/notes/notes-data'
+import patchouli_img from './assets/noteimg/Patchouli_img.webp'
+import sandalwood_img from './assets/noteimg/Sandalwood_img.webp'
+import juniper_img from './assets/noteimg/picture.webp'
+ 
 import './App.css'
 
 const App = () => {
@@ -12,6 +18,21 @@ const App = () => {
       />
       <ProductCard 
         perfumeObj={testPerfume}
+      />
+
+      <NoteTable 
+        notes={Patchouli}
+        pics={patchouli_img}
+      />
+
+      <NoteTable 
+        notes={Sandalwood}
+        pics={sandalwood_img}
+      />
+
+      <NoteTable 
+        notes={Juniper}
+        pics={juniper_img}
       />
     </>
   );
