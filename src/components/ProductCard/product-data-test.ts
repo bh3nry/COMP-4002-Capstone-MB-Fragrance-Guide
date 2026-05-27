0d21f2fc397f63e91e239c307e9017a822f02866
Daniel Henry <bfunk37@rrc.ca>
2026-05-26 19:22:43 -0500

    fix: updated test-data to follow Perfume type conventions

--- src/components/ProductCard/product-data-test.ts
+++ src/components/ProductCard/product-data-test.ts
@@ -1,4 +1,4 @@
-type Perfume = {
+export type Perfume = {
     title: string,
     notes: string,
     price: string
@@ -8,7 +8,7 @@ export type ProductCardProps = {
     perfumeObj: Perfume
 }
 
-export const testPerfume = {
+export const testPerfume: Perfume = {
     title: "V Good smell no.5",
     notes: "Juniper, Patchouli, Sandlewood",
     price: `$${49.99}`
