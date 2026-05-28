62540fd00ea5aa48500f671796a8b33c8034ded1
David Chan <dchan51@rrc.ca>
2026-05-28 15:59:53 -0500

    feat: changed css for notes-styles.
    - Created directory for favourites page with tsx and css.

---
+++ src/components/commons/favourites/Favourite.tsx
@@ -0,0 +1,11 @@
+function Favourites() {
+    return (
+        // <article>
+        //     <img src=""/>
+        //     <h1>Favourite Scents</h1>
+
+        // </article>
+    );
+}
+
+export default Favourites;
