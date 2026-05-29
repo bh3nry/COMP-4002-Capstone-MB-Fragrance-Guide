c5355969a8045378dccbf96a58d864fce5a5d005
Bren Henry <brendynfunk@gmail.com>
2026-05-29 09:37:01 -0500

    feat: Added state to the Home component, where state is updated as the user adds notes to the form element. Updated the JSX to reflect these changes.

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,8 +1,36 @@
+import React, { useState } from "react"
 
-const Home = () => {
+const Home = (): React.JSX.Element => {
+    
+    const [noteFromUser, setNoteFromUser] = useState<string[]>([]);
+
+    const getFormData = (formData: FormData): void => {
+        const formCollection = String(formData.get("userNotes"))
+        setNoteFromUser(x => [...x, formCollection])
+    }
+
+    const noteMap = noteFromUser.map((note, index) => (
+        <li key={index}>
+            {note ? String(note) : ""}
+        </li>
+    ))
+
     return (
         <div>
-            <p>Home page</p>
+            <h1>Home page</h1>
+            <form action={getFormData}>
+                <label htmlFor="userNotes">Notes:</label>
+                <input 
+                    id="userNotes" 
+                    name="userNotes" 
+                    type="text" 
+                    placeholder="Vanilla" 
+                    minLength={3}
+                    required
+                />
+                <button>Add New Note</button>
+            </form>
+            <ul> {noteMap} </ul>
         </div>
     )
 }
