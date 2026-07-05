7e5208ad46ed9c72424c00c63222e9f4da2bc2a8
Bren Henry <brendynfunk@gmail.com>
2026-07-05 14:20:40 -0500

    feat: Updated the backend to integrate express, and neon db. updated the prisma schema to use a simple table from neon that will eventually be called from the front end. Tested integration between prisma, neon, and express with success returning data from neon.

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -14,7 +14,11 @@ const app: Express = express()
 // Middleware
 app.use(cors(corsOptions));
 app.use(express.json())
-app.use(cors(corsOptions));
+
+// Health Check
+app.get('/', (_req: Request, res: Response) => {
+  res.send('Health Check, everything good?');
+});
 
 // Health Check
 app.get('/', (_req: Request, res: Response) => {
