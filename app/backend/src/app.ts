2543817ceb5463f0943a0f122ab9bd082bfd3927
Nicole Durrant-Eunson <ndurrant-eunson@academic.rrc.ca>
2026-07-04 19:01:06 -0500

    Added CORS configuration and set up Express server, created the app.ts and server.ts files for the backend

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -1,16 +1,19 @@
 import express, { type Express, type Request, type Response } from 'express';
 import dotenv from "dotenv";
+import cors from "cors";
 
 // env scoped to the backend
 dotenv.config({ path: './.env' });
 
 // Add local import statements here
 import scentRouter from './api/v1/routes/scentRoutes.ts';
+import corsOptions from "../config/cors";
 
 const app: Express = express();
 
 // Middleware
 app.use(express.json())
+app.use(cors(corsOptions));
 
 // Health Check
 app.get('/', (_req: Request, res: Response) => {
--- app/backend/src/server.ts
+++ app/backend/src/server.ts
@@ -1,9 +1,13 @@
 import app from "./app.ts"
 
+// get the server object from the http module
+import { Server } from "http";
+
 const PORT: string | number = process.env.PORT || 3000;
 
-const server = app.listen(PORT, () => {
-    console.log(`Server is listening on port: ${PORT}`)
-})
+// imported app listens for requests on given server
+const server: Server = app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
 export default server;
