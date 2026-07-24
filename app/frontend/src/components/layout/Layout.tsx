a083f90b0cdf5d3815c6ccda8fa535c93a16cf34
Nicole Durrant-Eunson <ndurrant-eunson@academic.rrc.ca>
2026-07-24 14:04:55 -0500

    Added clerk to Layout.tsx and Header.tsx, header has the sign in/out/user buttons

--- app/frontend/src/components/layout/Layout.tsx
+++ app/frontend/src/components/layout/Layout.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom"
 import Header from "../commons/header/Header"
+import { SignedIn, SignedOut, RedirectToSignIn } from "@clerk/clerk-react"
 
 const Layout = () => {
     return (
