8c8972f87729678aac71b8a6137e20935d67253c
Nicole Durrant-Eunson <ndurrant-eunson@academic.rrc.ca>
2026-07-24 14:07:05 -0500

    Removed from layout, imports don't need to be there

--- app/frontend/src/components/layout/Layout.tsx
+++ app/frontend/src/components/layout/Layout.tsx
@@ -1,6 +1,5 @@
 import { Outlet } from "react-router-dom"
 import Header from "../commons/header/Header"
-import { SignedIn, SignedOut, RedirectToSignIn } from "@clerk/clerk-react"
 
 const Layout = () => {
     return (
