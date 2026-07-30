c2ec0553bcc0cf2b8524e7582b065fee85b956dc
David Chan <dchan51@rrc.ca>
2026-07-30 18:11:23 -0500

    feat: created backend user management for finding/creating user Ids.

---
+++ app/backend/src/api/v1/middleware/findOrCreateUser.ts
@@ -0,0 +1,31 @@
+import * as userService from "../services/userService";
+import type { Request, Response, NextFunction } from "express";
+import { getAuth } from "@clerk/express";
+
+export interface AuthenticatedRequest extends Request {
+  userId?: string | null;
+}
+
+export const findOrCreateUser = async (
+    req: AuthenticatedRequest,
+    _res: Response,
+    next: NextFunction
+): Promise<void> => {
+    try {
+        const auth = getAuth(req);
+        const userId = auth.userId;
+
+        if(userId) {
+            let backendUser = await userService.getUserById(userId);
+            if(!backendUser) {
+                backendUser= await userService.createUser({id: userId});
+            }
+        }
+        
+        req.userId = userId;
+        next();
+
+    } catch(error) {
+        next(error);
+    }
+}
