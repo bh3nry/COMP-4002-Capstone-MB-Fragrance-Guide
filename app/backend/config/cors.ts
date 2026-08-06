84b05686f377bfc6607fa222a588950d2517163c
Bren Henry <brendynfunk@gmail.com>
2026-08-06 07:55:01 -0500

    Update allowed origins for CORS configuration

--- app/backend/config/cors.ts
+++ app/backend/config/cors.ts
@@ -4,7 +4,7 @@ import type { CorsOptions } from "cors";
 const corsOptions: CorsOptions = {
     // throw an error if the request does not come from the list of allowed origins
     origin: function(origin, callback) {
-        const allowedOrigins = [process.env.FRONTEND_URL];
+        const allowedOrigins = [process.env.PROD_FRONTEND_URL];
 
         // invoke callback (eg. next middleware) if  origin matches or no origin
         // some services (like postman) do not include an origin in their request
