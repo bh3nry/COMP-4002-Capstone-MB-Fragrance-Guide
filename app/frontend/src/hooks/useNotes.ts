1a8895e06482a0bd3327b00451f77b1e2d4033d6
David Chan <dchan51@rrc.ca>
2026-08-06 00:41:21 -0500

    feat: fixed favourites button.

--- app/frontend/src/hooks/useNotes.ts
+++ app/frontend/src/hooks/useNotes.ts
@@ -1,15 +1,17 @@
 import * as NoteService from "../services/favouriteService";
 import { useCallback, useEffect, useState } from "react";
+import { useAuth } from "@clerk/clerk-react";
 import type { FrontendNotes as Notes } from '@shared/types/frontend-notes';
 
 export function useNotes() {
-
+    const { getToken, isSignedIn } = useAuth();
     const [notes, setAllNotes] = useState<Notes[]>([]);
 
     const fetchNotes = useCallback(async () => {
-        const result = await NoteService.getNotes();
+        const sessionToken = isSignedIn ? await getToken() : null;
+        const result = await NoteService.getNotes(sessionToken);
         setAllNotes(result);
-    }, []);
+    }, [getToken, isSignedIn]);
 
     useEffect(() => {
         void fetchNotes();
